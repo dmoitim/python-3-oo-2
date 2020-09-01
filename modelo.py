04b34172db380afe34bb49d124c548ebd77f0dfc
Devair Luiz Moitim <dmoitim@gmail.com>
2020-09-01 19:11:02 -0300

    Implementação do polimorfismo simples

--- modelo.py
+++ modelo.py
@@ -19,11 +19,18 @@ class Programa:
     def nome(self, novo_nome):
         self._nome = novo_nome.title()
 
+    def imprimir(self):
+        pass
+
 
 class Filme(Programa):
     def __init__(self, nome, ano, duracao):
         super().__init__(nome, ano)
         self.duracao = duracao
+
+    def imprimir(self):
+        print(
+            f"{self._nome} ({self.ano}) - Duração: {self.duracao} min. - Likes: {self._likes}")
 
 
 class Serie(Programa):
@@ -31,12 +38,22 @@ class Serie(Programa):
         super().__init__(nome, ano)
         self.temporadas = temporadas
 
+    def imprimir(self):
+        print(
+            f"{self._nome} ({self.ano}) - Temporadas: {self.temporadas} - Likes: {self._likes}")
+
 
 vingadores = Filme('vingadores: guerra infinita', 2018, 160)
 vingadores.dar_like()
-print(f"{vingadores.nome} ({vingadores.ano}) - Duração: {vingadores.duracao} - Likes: {vingadores.likes}")
 
 vikings = Serie('vikings', 2013, 6)
 vikings.dar_like()
 vikings.dar_like()
-print(f"{vikings.nome} ({vikings.ano}) - Temporadas: {vikings.temporadas} - Likes: {vikings.likes}")
+
+filmes_e_series = [vingadores, vikings]
+
+for programa in filmes_e_series:
+    # detalhes = programa.duracao if (
+    #    hasattr(programa, 'duracao')) else programa.temporadas
+    #print(f"{programa.nome} ({programa.ano}) - {detalhes} - Likes: {programa.likes}")
+    programa.imprimir()
