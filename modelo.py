b9c578accdbc7de37264e7ad25f42853cfb7221d
Devair Luiz Moitim <dmoitim@gmail.com>
2020-09-01 19:42:46 -0300

    Remove herança da list para reduzir complexidade

--- modelo.py
+++ modelo.py
@@ -41,10 +41,18 @@ class Serie(Programa):
         return f"{self._nome} ({self.ano}) - Temporadas: {self.temporadas} - Likes: {self._likes}"
 
 
-class Playlist(list):
+class Playlist:
     def __init__(self, nome, programas):
         self.nome = nome
-        super().__init__(programas)
+        self._programas = programas
+
+    @property
+    def listagem(self):
+        return self._programas
+
+    @property
+    def tamanho(self):
+        return len(self._programas)
 
 
 vingadores = Filme('vingadores: guerra infinita', 2018, 160)
@@ -69,9 +77,7 @@ vikings.dar_like()
 filmes_e_series = [vingadores, vikings, demolidor, tmep]
 playlist_fim_de_semana = Playlist('fim de semana', filmes_e_series)
 
-print(f"Tamanho da playlist: {len(playlist_fim_de_semana)}")
+print(f"Tamanho da playlist: {playlist_fim_de_semana.tamanho}")
 
-for programa in playlist_fim_de_semana:
+for programa in playlist_fim_de_semana.listagem:
     print(programa)
-
-print(f"Demolidor está na lista? {demolidor in playlist_fim_de_semana}")
